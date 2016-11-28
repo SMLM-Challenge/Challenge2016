96a7054d9a76f81781fb2329fff0bbbca7ad5dcd
Hazen Babcock <hbabcock@fas.harvard.edu>
2016-11-28 14:03:31 -0500

    Fix to be compatible with Python3

--- file_conversion_utilities/tiffs_to_tiff.py
+++ file_conversion_utilities/tiffs_to_tiff.py
@@ -10,13 +10,13 @@ import sys
 import tifffile
 
 if (len(sys.argv) != 3):
-    print "usage: <tiff file> <tiff dir>"
+    print("usage: <tiff file> <tiff dir>")
     exit()
 
 tiff_files = sorted(glob.glob(sys.argv[2] + "*.tif"))
 
 with tifffile.TiffWriter(sys.argv[1]) as tif:
     for tiff_file in tiff_files:
-        print tiff_file
+        print(tiff_file)
         tiff_image = tifffile.imread(tiff_file)
         tif.save(tiff_image)
